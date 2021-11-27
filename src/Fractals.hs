db69a22fe01e80f1065ae5515dbdb8b817b24978
António Ramadas <antonio_ramadas@hotmail.com>
2021-11-27 18:43:40 +0000

    Add Koch snowflake
    More info: https://en.wikipedia.org/wiki/Koch_snowflake
    Based on tutorial from http://learn.hfm.io/fractals.html

--- src/Fractals.hs
+++ src/Fractals.hs
@@ -16,3 +16,29 @@ door = [(420, 750), (420, 550), (580, 550), (580, 750)]
 neonHouse :: Image PixelRGB8
 neonHouse = convertRGB8 . ImageRGBA8 $
                drawPicture 800 800 black 2.0 [(lightgreen, house), (red, door)]
+
+----------------- Koch snowflake -----------------
+kochLine :: Int -> Point -> Point -> Path
+kochLine n pS pE
+  | n <= 0 = []
+  | otherwise = [pS] ++ kochLine (n - 1) pS p1
+                     ++ kochLine (n - 1) p1 p2
+                     ++ kochLine (n - 1) p2 p3
+                     ++ kochLine (n - 1) p3 pE
+                     ++ [pE]
+  where
+    l1@(_, p1) = scaleLine (1 / 3) (pS, pE)
+    l2@(_, p3) = connectLine l1 l1
+    (_, p2) = rotateLine ((5 / 3) * pi) l2
+
+kochFlake :: Int -> Line -> Path
+kochFlake n line = kochLine n p1 p2
+                ++ kochLine n p2 p3
+                ++ kochLine n p3 p1
+  where
+    [p1, p2, p3, _] = polygon 3 line
+
+-- Example: mapM_ putSixel [snowflake n white blue | n <- [1..7]]
+snowflake :: Int -> Colour -> Colour -> Image PixelRGB8
+snowflake n colour backgroundColour = convertRGB8 . ImageRGBA8 $
+  drawPicture 800 800 backgroundColour 3.0 [(colour, kochFlake n ((100, 225), (700, 225)))]
--- src/LineGraphics.hs
+++ src/LineGraphics.hs
@@ -3,9 +3,9 @@
 module LineGraphics (
   Point, Vector, Line, Path, Picture, Colour,
   white, black, blue, red, green, orange, magenta, lightgreen, darkblue,
-  drawPicture,
+  connectLine, scaleLine, rotateLine, polygon, drawPicture,
 ) where
- 
+
 -- Rasterific
 import Graphics.Rasterific hiding (Point, Vector, Line, Path, polygon)
 import Graphics.Rasterific.Texture
@@ -21,7 +21,6 @@ type Picture = [(Colour, Path)]
 type Colour  = (Int, Int, Int, Int) -- red, green, blue, opacity
 
 -- Predefined colours
---
 white, black, blue, red, green, orange, magenta, lightgreen, darkblue :: Colour
 white      = (200,  200, 255, 255)
 black      = (  0,    0,   0, 255)
@@ -33,8 +32,53 @@ magenta    = (153,    0, 153, 220)
 lightgreen = ( 27,  230,  34, 255)
 darkblue   = ( 24,   50, 194, 255)
 
+-- Shifts a line to start at a given point
+startLineFrom :: Point -> Line -> Line
+startLineFrom startPoint@(x0, y0) ((xS, yS), (xE, yE))
+  = (startPoint, (x0 + xE - xS, y0 + yE - yS)) 
+
+-- Shifts second line to start where the first one ended
+connectLine :: Line -> Line -> Line
+connectLine (_, pE) line2 = startLineFrom pE line2
+
+-- Scale line given a scale factor (start point does not move)
+scaleLine :: Float -> Line -> Line
+scaleLine factor ((x1, y1), (x2, y2))
+  = ((x1, y1), (x' + x1, y' + y1))
+  where
+    x0 = x2 - x1
+    y0 = y2 - y1
+    x' = factor * x0
+    y' = factor * y0
+
+-- Rotates a line where the start point is the reference of the axis
+rotateLine :: Float -> Line -> Line
+rotateLine alpha ((x1, y1), (x2, y2))
+  = ((x1, y1), (x' + x1, y' + y1))
+  where
+    x0 = x2 - x1
+    y0 = y2 - y1
+    x' = x0 * cos alpha - y0 * sin alpha
+    y' = x0 * sin alpha + y0 * cos alpha
+
+-- Creates a spiral with n lines along a given angle
+spiral :: Float -> Float -> Int -> Line -> Path
+spiral angle scaleFactor n line
+  = spiral' n line
+  where
+    spiral' n line@(p1, p2)
+      | n <= 0    = []
+      | otherwise = p1 : spiral' (n - 1) newLine
+      where
+        newLine = connectLine line (scaleLine scaleFactor (rotateLine angle line))
+
+-- Creates a _circle_ with n lines
+polygon :: Int -> Line -> Path
+polygon n line | n > 2 = spiral rotationAngle 1 (n + 1) line
+  where 
+    rotationAngle = (2 * pi) / (fromIntegral n)
+
 -- Render a picture composed of coloured line paths with the specified line width.
---
 drawPicture :: Int -> Int -> Colour -> Float -> Picture -> Image PixelRGBA8
 drawPicture width height backgroundColour linewidth picture
   = renderDrawing width height (toColour backgroundColour) $
@@ -43,7 +87,3 @@ drawPicture width height backgroundColour linewidth picture
     drawPath points    = stroke linewidth  JoinRound (CapRound, CapStraight 0) $
                            polyline (map (uncurry V2) points)
     toColour (a,b,c,d) = PixelRGBA8 (fromIntegral a) (fromIntegral b) (fromIntegral c) (fromIntegral d)
-
-
-
-
