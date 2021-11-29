202d05ff4e94934fc19a1b961eacef98e93e720a
António Ramadas <antonio_ramadas@hotmail.com>
2021-11-29 17:47:17 +0000

    Add Hilbert space-filling curve
    More info: https://en.wikipedia.org/wiki/Hilbert_curve
    Based on paper:
    Breinholt, Greg & Schierz, Christoph. (1998).
    A recursive algorithm for the generation of space-filling curves.
    https://www.researchgate.net/publication/228982573_A_recursive_algorithm_for_the_generation_of_space-filling_curves

--- src/Fractals.hs
+++ src/Fractals.hs
@@ -47,17 +47,17 @@ snowflake n colour backgroundColour = convertRGB8 . ImageRGBA8 $
 fractalTree :: Float -> Int -> Line -> Path
 fractalTree factor n line = fractalTree' n line
   where
-    fractalTree' 0 line = []  
-    fractalTree' n line 
+    fractalTree' 0 line = []
+    fractalTree' n line
       = [p1, p4] ++ fractalTree' (n-1) (p4, p5) ++
                     fractalTree' (n-1) (p5, p3) ++
-        [p3, p2] 
-      where 
+        [p3, p2]
+      where
         -- flip direction of line
         flipLine :: Line -> Line
         flipLine (pS, pE) = (pE, pS)
 
-        -- The square: 
+        -- The square:
         --   4------3
         --   |      |
         --   |      |
@@ -68,7 +68,42 @@ fractalTree factor n line = fractalTree' n line
 
 -- Example: mapM_ putSixel [tree 0.5 340 120 n white blue | n <- [1..12]]
 -- Another: mapM_ putSixel [tree 0.6 400 120 n white blue | n <- [1..12]]
--- Possible improvement: We could change factor every ith iteration so the trees grow a bit taller 
+-- Possible improvement: We could change factor every ith iteration so the trees grow a bit taller
 tree :: Float -> Float -> Float -> Int -> Colour -> Colour -> Image PixelRGB8
 tree factor x width n color backgroundColour = convertRGB8 . ImageRGBA8 $
-    drawPicture 800 800 backgroundColour 3.0 [(color, fractalTree factor n ((x, 700), (x+width, 700)))]
+  drawPicture 800 800 backgroundColour 3.0 [(color, fractalTree factor n ((x, 700), (x+width, 700)))]
+
+----------------- Space-filling curve -----------------
+-- Line is bottom line from a square (left to right)
+hilbert :: Int -> Line -> Path
+hilbert n ((xS, yS), (xE, _)) = hilbert' n 0 0 (xE - xS) 0 0
+  where
+    hilbert' 1 xDelta yDelta side start end
+      | start == 0 && end == 0 = [q3, q2, q1, q4]
+      | start == 1 && end == 1 = [q1, q4, q3, q2]
+      | start == 0 && end == 1 = [q3, q4, q1, q2]
+      | start == 1 && end == 0 = [q1, q2, q3, q4]
+      where
+        headroom = side / 5
+        -- Quadrants
+        q1 = (xS + xDelta + side - headroom, yS - yDelta - side + headroom)
+        q2 = (xS + xDelta + headroom,        yS - yDelta - side + headroom)
+        q3 = (xS + xDelta + headroom,        yS - yDelta - headroom)
+        q4 = (xS + xDelta + side - headroom, yS - yDelta - headroom)
+
+    -- start = 0 -> bottom left
+    -- start = 1 -> upper right
+    -- end = 0 -> bottom right
+    -- end = 1 -> end left
+    hilbert' n' xDelta yDelta side start end =
+      hilbert' (n'-1) (xDelta + start * halfSide)     (yDelta + start * halfSide)     halfSide start     (1-end) ++
+      hilbert' (n'-1) (xDelta + end * halfSide)       (yDelta + (1-end) * halfSide)   halfSide start     end ++
+      hilbert' (n'-1) (xDelta + (1-start) * halfSide) (yDelta + (1-start) * halfSide) halfSide start     end ++
+      hilbert' (n'-1) (xDelta + (1-end) * halfSide)   (yDelta + end * halfSide)       halfSide (1-start) end
+      where
+        halfSide = side / 2
+
+-- Example: mapM_ putSixel [spaceFillingCurve (50, 750) 700 n white blue | n <- [1..7]]
+spaceFillingCurve :: Point -> Float -> Int -> Colour -> Colour -> Image PixelRGB8
+spaceFillingCurve (x, y) width n color backgroundColour = convertRGB8 . ImageRGBA8 $
+  drawPicture 800 800 backgroundColour 3.0 [(color, hilbert n ((x,y), (x+width, y)))]
